99910dd12fed445ff6b8f32467f83e2e1b2bb8e7
Peter Wallis <p.a.e.wallis@gmail.com>
2021-01-30 14:55:22 -0600

    Beach Object Data!
    * Added data files for beach objects
    * Data is now sent to beach objects when they're initialized
    * Beach objects can now be dug up (when in range)
    * Beach objects now have names (and know if they're valuable)

---
+++ Assets/Scripts/BeachObjectData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Beach/ObjectData")]
+public class BeachObjectData : ScriptableObject
+{
+    public string objectName;
+    public bool isValuable;
+}
+
+/*
+
+ITEM IDEAS
+
+Valuable:
+Generic Console
+Nokia flip phone
+A beer bottle
+A Melodica
+Childhood toy
+
+Not Valueable:
+Somebody's Microwave
+Rock
+Sea Shell
+Kelp/seaweed
+Needle
+An old coin
+Plastic bucket with a hole in it
+A plate of meatballs
+Garbage (cans, banana peel, broken glass)
+
+ */
--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -7,32 +7,44 @@ public class ObjectManager : MonoBehaviour
     public GameObject beachObjectPrefab;
     public PlayerController player;
     public int objectSpawnNumber;
-    [Range(1,20)]public float radius;
+    [Range(1,20)]public float scatterRadius;
+    [Range(0, 5)] public float playerProximityRadius;
+    public BeachObjectData[] valuableObjects;
+    public BeachObjectData[] nonValuableObjects;
 
     private List<BeachObject> beachObjects = new List<BeachObject>();
     private BeachObject closestObject = null;
 
     private void Start()
     {
+        BeachObjectData data;
         for (int i = 0; i < objectSpawnNumber; i++)
         {
             GameObject go = Instantiate(beachObjectPrefab, transform);
-            Vector2 pos = Random.insideUnitCircle * radius;
+            Vector2 pos = Random.insideUnitCircle * scatterRadius;
             go.transform.localPosition = new Vector3(pos.x, 0, pos.y);
 
+            if (i < valuableObjects.Length)
+                data = valuableObjects[i];
+            else
+                data = nonValuableObjects[Random.Range(0, nonValuableObjects.Length)];
+
             BeachObject bo = go.GetComponent<BeachObject>();
-            bo.id = i;
+            bo.Init(i,data);
             beachObjects.Add(bo);
         }
     }
 
-    private BeachObject GetClosestBeachObject()
+    private BeachObject GetClosestBuriedBeachObject()
     {
         BeachObject closest = null;
         float dist = float.MaxValue;
         for (int i = 0; i < beachObjects.Count; i++)
         {
-            float currentDist = Vector3.SqrMagnitude(beachObjects[i].position - player.transform.position);
+            if (beachObjects[i].WasDugUp) // Skip dug up items
+                continue;
+
+            float currentDist = Vector3.SqrMagnitude(beachObjects[i].position - player.position);
             if (currentDist < dist)
             {
                 dist = currentDist;
@@ -45,18 +57,30 @@ public class ObjectManager : MonoBehaviour
 
     private void Update()
     {
-        BeachObject newClosest = GetClosestBeachObject();
-        if (closestObject == null || newClosest.id != closestObject.id)
+        BeachObject newClosest = GetClosestBuriedBeachObject();
+        if (closestObject == null || newClosest.ID != closestObject.ID)
         {
-            if (closestObject != null)
+            if (closestObject != null && !closestObject.WasDugUp)
             {
                 closestObject.DebugSetColor(Color.yellow);
             }
             if (newClosest != null)
             {
-                newClosest.DebugSetColor(Color.blue);
                 closestObject = newClosest;
             }
+        }
+
+        if (closestObject != null)
+        {
+            float distance = Vector3.Distance(newClosest.position, player.position);
+            if (distance < playerProximityRadius)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                    closestObject.DigUp();
+                closestObject.DebugSetColor(closestObject.WasDugUp ? (closestObject.Data.isValuable ? Color.green : Color.blue) : Color.cyan);
+            }
+            else
+                closestObject.DebugSetColor(Color.gray);
         }
     }
 }
